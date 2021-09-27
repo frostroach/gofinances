3e6beffec36d908b0d66d506512315588fd26741
Vitor <vitor@fasters.com.br>
2021-09-27 11:02:19 -0300

    refactor: changes on categories config file

--- src/config/categories.ts
+++ src/config/categories.ts
@@ -2,31 +2,37 @@ import { Category } from "../models/category";
 
 export const categories: Category[] = [
   {
+    color: "#5636D3",
     name: "Casa",
     key: "home",
     icon: "home",
   },
   {
+    color: "#e83f5b",
     name: "Alimentação",
     key: "feeding",
     icon: "coffee",
   },
   {
+    color: "#FF872C",
     name: "Fatura",
     key: "cardBill",
     icon: "credit-card",
   },
   {
+    color: "#E83F5B",
     name: "Mercado",
     key: "market",
     icon: "shopping-cart",
   },
   {
+    color: "#969cb2",
     name: "Outros",
     key: "other",
     icon: "shopping-bag",
   },
   {
+    color: "#12a454",
     name: "Vendas",
     key: "sales",
     icon: "dollar-sign",
