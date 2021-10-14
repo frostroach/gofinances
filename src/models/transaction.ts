53bc104c809175de21a23a601a9672b4f5ab5fca
Vitor <vitor@fasters.com.br>
2021-10-14 08:17:03 -0300

    feat: add new models and change previous ones

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -2,6 +2,7 @@ import { Category } from "./category";
 
 export type Transaction = {
   id: string;
+  userId: string;
   title: string;
   value: number;
   category: Category;
---
+++ src/models/user.ts
@@ -0,0 +1,6 @@
+export type UserBase = {
+  id: string;
+  email: string;
+  name: string;
+  photo?: string;
+};
